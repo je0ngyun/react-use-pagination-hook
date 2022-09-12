abdffb4c513a033683d8cde74bf29501da9e2328
김정윤 <kjyjh9930@gmail.com>
2022-09-12 23:24:17 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 0,
+  },
+}
