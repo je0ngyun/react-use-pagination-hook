efa625b44540004e312fe270c6a2a603549dbe83
김정윤 <kjyjh9930@gmail.com>
2022-09-15 23:02:54 +0900

    test : hook params에 따라 리스트가 올바르게 표시되는지 테스트 #1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, within } from '@testing-library/react'
+import PagenationBar from './components/PagenationBar'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<PagenationBar />', () => {
+  const getPageList = () => {
+    return screen.getAllByRole('listitem')
+  }
+  it('Hook의 numOfPage 파라미터에 따라 페이지 리스트가 올바르게 표시되는지 테스트', () => {
+    const { rerender } = render(<PagenationBar numOfPage={5} totalPage={15} />)
+    expect(getPageList().map((item) => +item.textContent)).toStrictEqual([
+      1, 2, 3, 4, 5,
+    ])
+    rerender(<PagenationBar numOfPage={3} totalPage={15} />)
+    expect(getPageList().map((item) => +item.textContent)).toStrictEqual([
+      1, 2, 3,
+    ])
+  })
+})
