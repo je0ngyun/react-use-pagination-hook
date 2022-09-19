9727e4bd458a7ba6c788aa426147c513af4d4763
김정윤 <kjyjh9930@gmail.com>
2022-09-19 22:38:11 +0900

    modify : Demo용 PagenationBar Props 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <main className="container">
-        <PagenationBar numOfPage={3} totalPage={15} />
+        <PagenationBar numOfPage={5} totalPage={15} />
       </main>
     </div>
   )
