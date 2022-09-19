9ab9f2d5c6b3059be9167063aca0fb1efe981dca
김정윤 <kjyjh9930@gmail.com>
2022-09-19 22:01:33 +0900

    test : 테스트 컴포넌트 렌더 및 가상Dom 접근을 헬퍼 함수로 통합 #1

--- src/App.test.js
+++ src/App.test.js
@@ -1,63 +1,85 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import PagenationBar from './components/PagenationBar'
 
+const renderTestComponent = (props) => {
+  const { rerender: rerenderFunc } = render(<PagenationBar {...props} />)
+  const goLastButton = screen.getByRole('button', { name: /last/i })
+  const nextSectionButton = screen.getByRole('button', { name: /\>\>/i })
+  const goNextPageButton = screen.getByRole('button', { name: /^\>$/i })
+
+  const rerender = (props) => {
+    rerenderFunc(<PagenationBar {...props} />)
+  }
+
+  return {
+    rerender,
+    get pageList() {
+      return screen.getAllByRole('listitem')
+    },
+    get pageListValueArray() {
+      return screen.getAllByRole('listitem').map((item) => +item.textContent)
+    },
+    get selectedPageNum() {
+      return Number(screen.getByTestId('selected').textContent)
+    },
+    goLastButton,
+    nextSectionButton,
+    goNextPageButton,
+  }
+}
+
 describe('<PagenationBar />', () => {
-  const getPageList = () => {
-    return screen.getAllByRole('listitem')
-  }
-  const getGoLastButton = () => {
-    return screen.getByRole('button', { name: /last/i })
-  }
-  const getNextSectionButton = () => {
-    return screen.getByRole('button', { name: /\>\>/i })
-  }
-  const getGoNextPageButton = () => {
-    return screen.getByRole('button', { name: /^\>$/i })
-  }
-  const getSelectedPageNum = () => {
-    return Number(screen.getByTestId('selected').textContent)
-  }
-  it('Hook의 numOfPage 파라미터에 따라 페이지 리스트가 올바르게 표시되는지 테스트', () => {
-    const { rerender } = render(<PagenationBar numOfPage={5} totalPage={15} />)
-    expect(getPageList().map((item) => +item.textContent)).toStrictEqual([
-      1, 2, 3, 4, 5,
-    ])
-    rerender(<PagenationBar numOfPage={3} totalPage={15} />)
-    expect(getPageList().map((item) => +item.textContent)).toStrictEqual([
-      1, 2, 3,
-    ])
+  it('Hook의 numOfPage 파라미터에 따라 페이지 리스트가 올바르게 표시되어야 한다.', () => {
+    const helper = renderTestComponent({
+      numOfPage: 5,
+      totalPage: 15,
+    })
+    expect(helper.pageListValueArray).toStrictEqual([1, 2, 3, 4, 5])
+    helper.rerender({ numOfPage: 3, totalPage: 15 })
+    expect(helper.pageListValueArray).toStrictEqual([1, 2, 3])
   })
-  it('마지막 Section의 페이지 리스트 아이템들의 값 테스트', () => {
-    const { rerender } = render(<PagenationBar numOfPage={5} totalPage={15} />)
-    fireEvent.click(getGoLastButton())
-    expect(getPageList().map((item) => +item.textContent)).toStrictEqual([
-      11, 12, 13, 14, 15,
-    ])
-    rerender(<PagenationBar numOfPage={3} totalPage={9} />)
-    expect(getPageList().map((item) => +item.textContent)).toStrictEqual([
-      7, 8, 9,
-    ])
+  it('마지막 Section의 페이지 리스트 아이템들의 값은 totalPage - numOfPage - 1 부터 numOfPage 사이즈만큼 순차적으로 커져야한다', () => {
+    const helper = renderTestComponent({
+      numOfPage: 5,
+      totalPage: 15,
+    })
+    fireEvent.click(helper.goLastButton)
+    expect(helper.pageListValueArray).toStrictEqual([11, 12, 13, 14, 15])
+    helper.rerender({
+      numOfPage: 3,
+      totalPage: 9,
+    })
+    expect(helper.pageListValueArray).toStrictEqual([7, 8, 9])
   })
-  it('totalPage % numOfPage !== 0 일때 마지막 Section의 페이지 리스트 사이즈는 나머지와 같아야한다.', () => {
-    render(<PagenationBar numOfPage={5} totalPage={13} />)
-    fireEvent.click(getGoLastButton())
-    expect(getPageList().length).toBe(13 % 5)
+  it('totalPage % numOfPage !== 0 일때 마지막 Section의 페이지 리스트 사이즈는 totalPage % numOfPage와 같아야한다.', () => {
+    const helper = renderTestComponent({
+      numOfPage: 5,
+      totalPage: 13,
+    })
+    fireEvent.click(helper.goLastButton)
+    expect(helper.pageList.length).toBe(13 % 5)
   })
-  it('section 이동버튼 클릭시의 리스트 아이템들의 값 및 선택처리된 값 테스트', () => {
+  it('다음 section으로 이동할때마다 리스트 아이템들의 값은 이전페이지의 각 아이템 + numOfPage 만큼 커져야한다.', () => {
     const initPages = [1, 2, 3, 4, 5]
-    render(<PagenationBar numOfPage={5} totalPage={15} />)
+    const helper = renderTestComponent({
+      numOfPage: 5,
+      totalPage: 15,
+    })
     for (let i = 1; i < 3; i++) {
-      fireEvent.click(getNextSectionButton())
-      expect(getPageList().map((item) => +item.textContent)).toStrictEqual(
+      fireEvent.click(helper.nextSectionButton)
+      expect(helper.pageListValueArray).toStrictEqual(
         initPages.map((pageNum) => pageNum + i * 5)
       )
     }
   })
-  it('페이지 이동 엑션에 따른 현재 페이지 번호 테스트', () => {
-    render(<PagenationBar numOfPage={5} totalPage={15} />)
+  it('다음 페이지 이동 호출시 현재 페이지는 이전 값 + 1 이 되어야 한다.', () => {
+    const helper = renderTestComponent({
+      numOfPage: 5,
+      totalPage: 15,
+    })
     for (let i = 1; i < 16; i++) {
-      expect(getSelectedPageNum()).toBe(i)
-      fireEvent.click(getGoNextPageButton())
+      expect(helper.selectedPageNum).toBe(i)
+      fireEvent.click(helper.goNextPageButton)
     }
   })
 })
