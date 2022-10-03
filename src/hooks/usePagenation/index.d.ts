720a95ca220f5da80f06b53cebb22cf995290a0b
김정윤 <kjyjh9930@gmail.com>
2022-10-03 22:18:59 +0900

    feat : typescript를 위한 d.ts파일 작성 #3

--- src/hooks/usePagenation/index.d.ts
+++ src/hooks/usePagenation/index.d.ts
@@ -1,1 +1,21 @@
-export {}
+interface Params {
+  numOfPage: number
+  totalPage: number
+}
+
+declare const usePagenation: ({ numOfPage, totalPage }: Params) => {
+  pagelist: number[]
+  goNextSection: () => void
+  goBeforeSection: () => void
+  goFirstSection: () => void
+  goLastSection: () => void
+  goNext: () => void
+  goBefore: () => void
+  setTotalPage: (tatalPage: number) => void
+  setPage: (page: number) => void
+  hasNextSection: boolean
+  hasBeforeSection: boolean
+  currentPage: number
+}
+
+export default usePagenation
