82c0120c522fa171c215f9c2cc41918cc05ee361
김정윤 <kjyjh9930@gmail.com>
2022-10-14 23:25:29 +0900

    feat : 데모페이지에 Hook의 currentPage 값을 확인 할 수 있게 예시 페이지 수정 및 Props값을 바꿔볼수 있는 기능 추가 #9

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
 import PaginationBar from './components/PaginationBar'
 import './index.css'
 
 const App = () => {
+  const [currentPage, setCurrentPage] = useState()
+  const [numOfPage, setNumOfPage] = useState(5)
+  const [totalPage, setTotalPage] = useState(15)
+
+  const handleNumOfPageFieldChange = ({ target: { value } }) => {
+    setNumOfPage(+value)
+  }
+
+  const handleTotalPageFieldChange = ({ target: { value } }) => {
+    setTotalPage(+value)
+  }
+
   return (
     <div className="App">
-      <main className="container">
-        <PaginationBar numOfPage={5} totalPage={15} />
+      <img
+        src="https://raw.githubusercontent.com/je0ngyun/react-use-pagination-hook/master/media/logo.png"
+        alt="logo"
+      />
+      <div className="input">
+        <label>currentPage : </label>
+        <div>{currentPage}</div>
+      </div>
+      <div className="input">
+        <label htmlFor="num-of-page">numOfPage : </label>
+        <input
+          min={1}
+          value={numOfPage}
+          onChange={handleNumOfPageFieldChange}
+          id="num-of-page"
+          type="number"
+        />
+      </div>
+      <div className="input">
+        <label htmlFor="total-page">totalPage : </label>
+        <input
+          value={totalPage}
+          onChange={handleTotalPageFieldChange}
+          id="total-page"
+          type="number"
+        />
+      </div>
+      <main>
+        <PaginationBar
+          onChange={setCurrentPage}
+          numOfPage={numOfPage}
+          totalPage={totalPage}
+        />
       </main>
     </div>
   )
--- src/components/PaginationBar.jsx
+++ src/components/PaginationBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import usePagination from '../hooks/usePagination'
 import PropTypes from 'prop-types'
+import { useEffect } from 'react'
 
-const PaginationBar = ({ numOfPage, totalPage }) => {
+const PaginationBar = ({ numOfPage, totalPage, onChange }) => {
   const {
     pagelist,
     goNextSection,
@@ -16,28 +17,30 @@ const PaginationBar = ({ numOfPage, totalPage }) => {
     currentPage,
   } = usePagination({ numOfPage, totalPage })
 
+  useEffect(() => {
+    onChange(currentPage)
+  }, [currentPage])
+
   return (
-    <div className="App">
-      <main className="container">
-        <button onClick={() => goFirstSection()}>{'First'}</button>
-        <button onClick={() => goBeforeSection()}>{'<<'}</button>
-        <button onClick={() => goBefore()}>{'<'}</button>
-        <ul className="pages" aria-labelledby="pages">
-          {pagelist.map((page) => (
-            <li
-              onClick={() => setPage(page)}
-              className={currentPage === page ? 'selected' : ''}
-              data-testid={currentPage === page ? 'selected' : ''}
-              key={page}
-            >
-              {page}
-            </li>
-          ))}
-        </ul>
-        <button onClick={() => goNext()}>{'>'}</button>
-        <button onClick={() => goNextSection()}>{'>>'}</button>
-        <button onClick={() => goLastSection()}>{'Last'}</button>
-      </main>
+    <div className="container">
+      <button onClick={() => goFirstSection()}>{'First'}</button>
+      <button onClick={() => goBeforeSection()}>{'<<'}</button>
+      <button onClick={() => goBefore()}>{'<'}</button>
+      <ul className="pages" aria-labelledby="pages">
+        {pagelist.map((page) => (
+          <li
+            onClick={() => setPage(page)}
+            className={currentPage === page ? 'selected' : ''}
+            data-testid={currentPage === page ? 'selected' : ''}
+            key={page}
+          >
+            {page}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => goNext()}>{'>'}</button>
+      <button onClick={() => goNextSection()}>{'>>'}</button>
+      <button onClick={() => goLastSection()}>{'Last'}</button>
     </div>
   )
 }
